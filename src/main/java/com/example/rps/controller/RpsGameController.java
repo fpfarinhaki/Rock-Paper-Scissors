bd1eab2f54f9638481e9f0a138e9c745d540ac4d
felipe <fpfarinhaki.atc@gmail.com>
2019-10-02 09:45:13 +0200

    Allow Cross Origin from localhost GUI

--- src/main/java/com/example/rps/controller/RpsGameController.java
+++ src/main/java/com/example/rps/controller/RpsGameController.java
@@ -3,15 +3,13 @@ package com.example.rps.controller;
 import com.example.rps.domain.GameSession;
 import com.example.rps.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/games/rps")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RpsGameController {
 
     @Autowired
